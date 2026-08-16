bb29c2e899976ec6ecdf35002e0f1c70616fcae4
yanlvl99 <hryan5192@gmail.com>
2026-08-16 01:47:01 -0300

    sys: add operating system signal watchers (#101)

---
+++ modules/sys/include/Sys/SysSignals.hpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2026 yanlvl99, Lode Runtime Contributors
+// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "Lode/Signal.hpp"
+#include "Lode/State.hpp"
+#include "Lode/Table.hpp"
+#include "Lode/Value.hpp"
+#include "uv.h"
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace lodesys
+{
+
+struct SignalWatcher;
+
+struct SignalManager : std::enable_shared_from_this<SignalManager>
+{
+    Lode::State* vm = nullptr;
+    bool shuttingDown = false;
+    std::unordered_map<std::string, std::shared_ptr<SignalWatcher>> watchers;
+
+    Lode::Value Watch(Lode::State& state, const std::vector<Lode::Value>& args);
+    void Shutdown();
+};
+
+void BindSysSignals(Lode::State& vm, Lode::Table& exports);
+
+} // namespace lodesys
